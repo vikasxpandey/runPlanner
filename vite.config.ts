00fba67d9924bbd913141c953b414cac97ad1c51
Vikas Pandey <vikas.pandey792@gmail.com>
2026-03-15 17:49:55 +0530

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "runPlanner",
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] }),
